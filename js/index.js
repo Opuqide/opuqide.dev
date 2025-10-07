eba5efe6379f653b3b88efb1696036812aebf08d
Tyler Fulgham <tylerfulgham9@gmail.com>
2025-10-07 13:50:42 -0500

    All files commited.

---
+++ js/index.js
@@ -0,0 +1,92 @@
+var templates = new Object();
+
+function fillTab(tab, tabContents) {
+    tabContents.innerHTML = Mustache.render(templates[tab.map_id], map[tab.map_id]);
+    tab.inited = 1;
+}
+
+function tabHandler(event) {
+    var requestedTab = event.srcElement;
+    var activeTab = document.getElementById("tabs").getElementsByClassName("tab-active")[0];
+
+    if (requestedTab == activeTab) {
+        return;
+    }
+
+    if (activeTab) {
+        activeTab.classList.remove("tab-active");
+        document.getElementById(activeTab.id + "-contents").hidden = true;
+    }
+
+    requestedTab.classList.add("tab-active");
+
+    var requestedTabContents = document.getElementById(requestedTab.id + "-contents");
+    requestedTabContents.hidden = false;
+
+    if (!requestedTab.inited) {
+        fillTab(requestedTab, requestedTabContents);
+    }
+}
+
+function templateCallback(xhr, tab) {
+    if (xhr.readyState != 4) { 
+        return;
+    }
+
+    if (xhr.status == 200) {
+        templates[tab.map_id] = xhr.responseText;
+
+        tab.onclick = tabHandler;
+
+        if (tab.getAttribute("primary")) {
+            tabHandler({srcElement : tab});
+        }
+    } else {
+        alert("Wow, this is unexpected, but we really couldn't load " + key + " template!");
+    }
+}
+
+function iOS() {
+    return !!navigator.platform.match(/iPhone|iPod|iPad/);
+}
+
+var colors = [
+    "#e41f57ff",
+    "#832c5dff",
+    //"#06402b",
+    "#91043c"
+];
+
+function setBackground() {
+    var timestamp = Math.floor(Date.now() / 1000);
+    var color = colors[timestamp % colors.length];
+
+    var gradient = "radial-gradient(circle at bottom, " + color + ", black)";
+    var bodyElement = document.body;
+
+    if (!iOS()) {
+        bodyElement.style["background"] = gradient;
+        bodyElement.style["background-attachment"] = "fixed";
+    } else {
+        var bgElement = document.createElement("div");
+        bgElement.className = "fixed-bg";
+        bgElement.style["background"] = gradient;
+        bodyElement.prepend(bgElement);
+    }
+}
+
+function main() {
+    setBackground();
+
+    for (var key in map) {
+        var element = map[key];
+        var tab = document.getElementById(key + "-tab");
+        tab.map_id = key;
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("GET", element["template"], true);
+        xhr.onreadystatechange = templateCallback.bind(null, xhr, tab);
+        xhr.send();
+    }
+}
---
+++ map/map.js
@@ -0,0 +1,56 @@
+var map =
+{
+    "projects" : {
+        "template" : "templates/project.html",
+        "desc" : "All of the projects ive done in my life",
+        "posts" : []
+    },
+
+    "articles" : {
+        "template" : "templates/article.html?v=2",
+        "desc" : "articles that ive written for no reason -w-",
+        "posts" : [
+            {
+                "title" : "Project Alpine: Remastered",
+                "date" : "2.17.25",
+                "desc" : "An article on the iPhone in Roblox",
+                "picture" : "resources/articles/project-alpine.png",
+                "link" : "par",
+                "pinned" : true
+            },
+        ]
+    },
+
+    "threads" : {
+        "template" : "templates/thread.html?v=1",
+        "desc" : "Twitter threads - I write them if the information is too little to have its' own article",
+        "posts" : []
+    },
+
+    "links" : {
+        "template" : "templates/link.html?v=1",
+        "posts" : [
+            {
+                "name" : "Opuqide",
+                "link" : "https://twitter.com/Opuqide",
+                "icon" : "resources/ui/twitter_logo.png",
+                "desc" : "not smth that i post on",
+                "atprefix" : true
+            },
+            {
+                "name" : "Opuqide",
+                "link" : "https://youtube.com/@Opuqide",
+                "icon" : "resources/ui/twitter_logo.png",
+                "desc" : "i sometimes post on there",
+                "atprefix" : true
+            },
+            {
+                "name" : "Opuqide",
+                "link" : "https://devforum.roblox.com/u/opuqide/summary",
+                "icon" : "resources/ui/studio.png",
+                "desc" : "i sometimes post on there",
+                "atprefix" : true
+            },
+        ]
+    }
+};
