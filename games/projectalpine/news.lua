b429398f97d032ff39e4a48dfd9d6550202f782e
Tyler <101429436+Opuqide@users.noreply.github.com>
2026-04-06 17:33:56 -0500

    Create news list for Project Alpine.

---
+++ games/projectalpine/news.lua
@@ -0,0 +1,11 @@
+-- Copyright (C) 2026 Project Alpine. Authorized for public release.
+
+return {
+  {
+    Name = "Test News Release",
+    ShortDescription = "uhhhhh this is a test."
+    Description = "",
+    DisableReading = true,
+    Time = 1775514819
+  }
+}
